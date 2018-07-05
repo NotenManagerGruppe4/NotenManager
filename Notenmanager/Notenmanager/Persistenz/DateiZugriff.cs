7fd56012cf8fa99d14007b200ac7ddfb370fa6ba
Köhler <2016koehlerc@edvw.de>
2018-07-05 11:07:07 +0200

    Methode LeseDatei angelegt

--- Notenmanager/Notenmanager/Persistenz/DateiZugriff.cs
+++ Notenmanager/Notenmanager/Persistenz/DateiZugriff.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Notenmanager.Persistenz
 {
-    class DateiZugriff
+    public static class DateiZugriff
     {
+        public static string[] LeseDatei(string pfad)
+        {
+            return File.ReadAllLines(pfad);
+        }
+
+        public static void ImportSchueler(string pfad)
+        {
+
+        }
     }
 }
