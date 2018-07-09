80ccd5ee511bfefc146a0e5dfc52961c6b96177d
Stich <2016stichj@edvw.de>
2018-07-09 12:20:40 +0200

    Arbeiten an FachAnlegenPage

--- Notenmanager/Notenmanager/ViewModel/FachAnlegenPageVM.cs
+++ Notenmanager/Notenmanager/ViewModel/FachAnlegenPageVM.cs
@@ -5,27 +5,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Notenmanager.Model;
+using System.Windows.Input;
 
 namespace Notenmanager.ViewModel
 {
     public enum Fachart { Wahlfach = 1, Pflichtfach = 2, Wahlpflichtfach = 3};
+    public enum DialogMode { Neu, Ändern};
 
     public class FachAnlegenPageVM : BaseViewModel
     {
-        private ObservableCollection<Unterrichtsfach> _lstUFach = new ObservableCollection<Unterrichtsfach>();
+        private ObservableCollection<Unterrichtsfach> _lstZeugUFach = new ObservableCollection<Unterrichtsfach>();
+        private Unterrichtsfach _selFach;
+        private ObservableCollection<Unterrichtsfach> _lstUFachHinz = new ObservableCollection<Unterrichtsfach>();
 
-        public ObservableCollection<Unterrichtsfach> LstUFach
+        public event EventHandler<DialogEventArgs> DialogRequest;
+        public ICommand OnBtnAnlegenCmd { get; set; }
+
+        public FachAnlegenPageVM()
+        {
+            OnBtnAnlegenCmd = new ActionCommand(OnBtnAnlegen);
+        }
+        public ObservableCollection<Unterrichtsfach> LstZeugUFach
         {
             get
             {
-                return _lstUFach;
+                return _lstZeugUFach;
             }
 
             set
             {
-                _lstUFach = value;
-                onPropertyChanged("LstUFach");
+                _lstZeugUFach = value;
+                onPropertyChanged("LstZeugUFach");
             }
+        }
+
+        public Unterrichtsfach SelFach
+        {
+            get
+            {
+                return _selFach;
+            }
+
+            set
+            {
+                _selFach = value;
+                onPropertyChanged("SelFach");
+            }
+        }
+
+        public ObservableCollection<Unterrichtsfach> LstUFachHinz
+        {
+            get
+            {
+                return _lstUFachHinz;
+            }
+
+            set
+            {
+                _lstUFachHinz = value;
+                onPropertyChanged("LstUFachHinz");
+            }
+        }
+
+        private void OnBtnAnlegen(object obj)
+        {
+            DialogRequest?.Invoke(this, new DialogEventArgs(DoAnlegen, DialogMode.Neu));
         }
     }
 }
