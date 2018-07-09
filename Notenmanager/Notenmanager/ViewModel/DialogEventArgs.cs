fb47e2f74c285813d2af53dfe5b7249631f8de39
Stich <2016stichj@edvw.de>
2018-07-09 12:25:56 +0200

    fixed untracked files

---
+++ Notenmanager/Notenmanager/ViewModel/DialogEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notenmanager.ViewModel
+{
+    public class DialogEventArgs
+    {
+        public Action<bool?> ResultAction { get; set; }
+        public DialogMode dm;
+
+        public DialogEventArgs(Action<bool?> resultAction, DialogMode dm)
+        {
+            this.dm = dm;
+            ResultAction = resultAction;
+        }
+    }
+}
