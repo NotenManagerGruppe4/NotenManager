329e01d36df7fa2719b25c471dc9eca36d6c3e69
Ducheck <2016ducheckj@edvw.de>
2018-07-09 09:54:13 +0200

    DateiImportPage

---
+++ Notenmanager/Notenmanager/View/DateiImportPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notenmanager.View
+{
+    /// <summary>
+    /// Interaktionslogik für DateiImportPage.xaml
+    /// </summary>
+    public partial class DateiImportPage : Page
+    {
+        public DateiImportPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFileSelect_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAbbrechen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
