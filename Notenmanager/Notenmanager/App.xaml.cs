be6e68ed43fdfdb646e27d8884b7c94a7f116d6c
Bierler <2016bierlera@edvw.de>
2018-07-11 14:52:41 +0200

    FIX + Tests

--- Notenmanager/Notenmanager/App.xaml.cs
+++ Notenmanager/Notenmanager/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +21,12 @@ namespace Notenmanager
 
          DBZugriff.InitDB();
 
+         if (Environment.GetCommandLineArgs().Contains("-test"))
+         {
+            Tests s = new Tests();
+            s.Test2();
+         }
+
          AppDomain.CurrentDomain.UnhandledException += (sender, e2) =>
          {
             DBZugriff.CloseDB();
@@ -29,6 +36,133 @@ namespace Notenmanager
       private void Application_Exit(object sender, ExitEventArgs e)
       {
          DBZugriff.CloseDB();
+      }
+
+      //[TestClass()]
+      public class Tests
+      {
+
+         //[TestMethod()]
+         public void Test2()
+         {
+            try
+            {
+               //DBZugriff.InitDB();
+
+               InsertTest();
+            }
+            catch (Exception e)
+            {
+               Trace.WriteLine(e.ToString());
+               //Assert.Fail();
+            }
+            //DBZugriff.CloseDB();
+         }
+
+
+         private static void InsertTest()
+         {
+
+            Schule schule = new Schule()
+            {
+               Bez = "TestSchule",
+            };
+            schule.Speichern();
+
+            Lehrer leh = new Lehrer()
+            {
+               Vorname = "VorTest",
+               Nachname = "NachTest",
+               Kürzel = "TL",
+            };
+            leh.Speichern();
+
+            Klasse kl = new Klasse()
+            {
+               Bez = "TKL1A",
+               SJ = 2017,
+               Schule = schule,
+               Klassenleiter = leh,
+               StellvertretenderKlassenleiter = leh,
+            };
+            kl.Speichern();
+
+            Zeugnisfach zf = new Zeugnisfach()
+            {
+               Bez = "TestAbschlussFach",
+               Fachart = "TestFachArt",
+               Pos = 1,
+               Vorrueckungsfach = true,
+               AbschliessendesFach = true,
+               Klasse = kl,
+            };
+            zf.Speichern();
+
+            Unterrichtsfach uf = new Unterrichtsfach()
+            {
+               Bez = "TestUnterichtsFach",
+               Pos = 1,
+               Stunden = 6,
+               Zeugnisfach = zf,
+            };
+            uf.Speichern();
+
+            UFachLehrer ufl = new UFachLehrer()
+            {
+               Lehrer = leh,
+               Unterrichtsfach = uf,
+               Stunden = uf.Stunden
+            };
+            ufl.Speichern();
+
+
+            Schueler s = new Schueler()
+            {
+               Vorname = "VorTestS",
+               Nachname = "NachTestS",
+               Geburtsdatum = new DateTime(2000, 1, 1),
+               Geschlecht = Geschlecht.M,
+               Konfession = Konfession.BL,
+            };
+            s.Speichern();
+
+            SchuelerKlasse sk = new SchuelerKlasse()
+            {
+               Klasse = kl,
+               Schueler = s,
+            };
+            sk.Speichern();
+
+
+            Leistungsgruppe lg = new Leistungsgruppe()
+            {
+               Bez = "Schriftlich",
+               Gewicht = 2,
+            };
+            lg.Speichern();
+
+            Leistungsart la = new Leistungsart()
+            {
+               Bez = "Schulaufgabe",
+               Gewichtung = 2,
+               Gruppe = lg,
+
+            };
+            la.Speichern();
+
+            Leistung lei = new Leistung()
+            {
+               Erhebungsdatum = DateTime.Now,
+               Notenstufe = 1,
+               Tendenz = Tendenz.UP,
+               LetzteÄnderung = DateTime.Now,
+               Leistungsart = la,
+               SchuelerKlasse = sk,
+               UFachLehrer = ufl
+            };
+            lei.Speichern();
+
+         }
       }
    }
 }
