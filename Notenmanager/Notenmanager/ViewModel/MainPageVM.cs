f366c40cc9715dad076b063d188b54e18e49a3c5
Christoph Koehler <Christoph.Koehler97@web.de>
2018-07-15 21:05:21 +0200

    initial commit to fix of navigation

--- Notenmanager/Notenmanager/ViewModel/MainPageVM.cs
+++ Notenmanager/Notenmanager/ViewModel/MainPageVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Notenmanager.ViewModel
@@ -14,6 +15,7 @@ namespace Notenmanager.ViewModel
         {
             NavigateToDateiImportCmd = new ActionCommand(OnNavigateToDateiImport);
             NavigateToFachAnlegenCmd = new ActionCommand(OnNavigateToFachAnlegen);
+            NavigationCmd = new Command<string>(OnNavigation);
         }
 
 
@@ -25,17 +27,25 @@ namespace Notenmanager.ViewModel
         #region Commands
         public ICommand NavigateToDateiImportCmd { get; set; }
         public ICommand NavigateToFachAnlegenCmd { get; set; }
+        public ICommand NavigationCmd { get; set; }
+
         #endregion
         #endregion
 
         #region Methoden
         #region CommandHandler
+        private static void OnNavigation(string key)
+        {
+            (App.Current.FindResource("MainWindowVM") as MainWindowVM).CurrentPage = App.Current.FindResource(key) as Page;
+        }
+
+
         private void OnNavigateToDateiImport(object obj)
         {
             NavigateToPageRequest?.Invoke(this, new NavigationEventArgs()
             {
                 ZielPage = new DateiImportPage()
-            });            
+            });
         }
         private void OnNavigateToFachAnlegen(object obj)
         {
