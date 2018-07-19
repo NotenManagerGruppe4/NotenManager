9c51f30f9259849d021ca8e76fa7b0d6a5d7b76a
Stich <2016stichj@edvw.de>
2018-07-19 08:47:41 +0200

    Änderungen

--- Notenmanager/Notenmanager/ViewModel/LehrerAuswahlWindowVM.cs
+++ Notenmanager/Notenmanager/ViewModel/LehrerAuswahlWindowVM.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Notenmanager.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,6 @@ namespace Notenmanager.ViewModel
 {
     public class LehrerAuswahlWindowVM
     {
+        private ObservableCollection<Lehrer> lehrerListe = new ObservableCollection<Lehrer>(DBZugriff.Current.Select<Lehrer>());
     }
 }
